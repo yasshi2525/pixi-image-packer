737c34b3114aa87a12e19c92d1fd655c1d5a6fd9
yasshi2525 <yasshy2@gmail.com>
2023-02-06 06:28:46 +0900

    クロール中のエラーで停止しないよう修正

--- src/cli/crawler.ts
+++ src/cli/crawler.ts
@@ -27,42 +27,46 @@ export class Crawler {
       return Promise.resolve()
     }
     console.log('crawling images...')
-    const browser = await puppeteer.launch({
-      headless: true, timeout: this.timeout
-    })
-    const page = await browser.newPage()
-    page.setDefaultTimeout(this.timeout)
-    await page.goto(`http://localhost:${this.port}/?download=1`)
-    await page.waitForSelector('a')
+    try {
+      const browser = await puppeteer.launch({
+        headless: true, timeout: this.timeout
+      })
+      const page = await browser.newPage()
+      page.setDefaultTimeout(this.timeout)
+      await page.goto(`http://localhost:${this.port}/?download=1`)
+      await page.waitForSelector('a')
 
-    const ls = await page.evaluate(() => {
-      const list = [...document.querySelectorAll('a')]
-      return list.map(a => ({ name: a.download, data: a.href }))
-    })
+      const ls = await page.evaluate(() => {
+        const list = [...document.querySelectorAll('a')]
+        return list.map(a => ({ name: a.download, data: a.href }))
+      })
 
-    if (fs.existsSync(this.outPath)) {
-      fs.rmSync(this.outPath, { recursive: true, force: true })
-    }
-    ls.forEach(data => {
-      const dirIdx = data.name.lastIndexOf('/')
-      const outDir = (dirIdx === -1) ? this.outPath : path.join(this.outPath, data.name.substring(0, dirIdx))
-      fs.mkdirSync(outDir, { recursive: true })
-
-      if (data.name.endsWith('.json')) {
-        fs.writeFileSync(
-          path.join(this.outPath, data.name),
-          decodeURIComponent(data.data.replace(/^data:text\/json;charset=utf-8,/, ''))
-        )
-      } else {
-        fs.writeFileSync(
-          path.join(this.outPath, data.name),
-          data.data.replace(/^data:image\/png;base64,/, ''),
-          'base64'
-        )
+      if (fs.existsSync(this.outPath)) {
+        fs.rmSync(this.outPath, { recursive: true, force: true })
       }
-    })
-    await browser.close()
-    console.log(`saved images to ${this.outPath}`)
+      ls.forEach(data => {
+        const dirIdx = data.name.lastIndexOf('/')
+        const outDir = (dirIdx === -1) ? this.outPath : path.join(this.outPath, data.name.substring(0, dirIdx))
+        fs.mkdirSync(outDir, { recursive: true })
+
+        if (data.name.endsWith('.json')) {
+          fs.writeFileSync(
+            path.join(this.outPath, data.name),
+            decodeURIComponent(data.data.replace(/^data:text\/json;charset=utf-8,/, ''))
+          )
+        } else {
+          fs.writeFileSync(
+            path.join(this.outPath, data.name),
+            data.data.replace(/^data:image\/png;base64,/, ''),
+            'base64'
+          )
+        }
+      })
+      await browser.close()
+      console.log(`saved images to ${this.outPath}`)
+    } catch (e) {
+      console.error(e)
+    }
     if (this.downloadQueue > 0) {
       this.downloadQueue--
       await this.download()
