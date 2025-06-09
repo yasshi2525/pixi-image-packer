22a38667c7e00175d4465ef1933c64a8ac353e83
yasshi2525 <yasshy2@gmail.com>
2025-06-09 21:47:02 +0900

    フォントのロード方法を修正

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -2,7 +2,7 @@ import { AssetsParameters } from './assets'
 import { application } from './application'
 import { HotReload } from './hotreload'
 import { draw } from './draw'
-import { Assets, BitmapFont } from 'pixi.js'
+import { Assets } from 'pixi.js'
 
 type ConsumerOption = {
   assets: () => Promise<AssetsParameters>,
@@ -14,11 +14,12 @@ type ConsumerOption = {
 const main = async (opts: ConsumerOption) => {
   new HotReload({ port: opts.port }).start()
   const app = await application()
-  Assets.addBundle('fonts', opts.fonts.map(f => ({ alias: f.name, src: f.url })))
+  Assets.addBundle('fonts', opts.fonts.map(f => ({
+    alias: f.name,
+    src: f.url,
+    data: { family: f.name }
+  })))
   await Assets.loadBundle('fonts')
-  for (const font of opts.fonts) {
-    BitmapFont.install({ name: `${font.name}-bitmap`, style: { fontFamily: font.name } })
-  }
   const assets = await opts.assets()
   // null 出現以降は無視
   if (assets.indexOf(null) !== -1) {
